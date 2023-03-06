05fd325b8f70436ba11c55250ec6742542e6bea2
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2023-03-06 21:54:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Index from './components/Index';
-// import Login from './components/Login';
+import Login from './components/Login';
 function App() {
   return (
     <>
-    {/* <Login/> */}
-    <Index/>
+    <Login/>
+    {/* <Index/> */}
     </>
   );
 }
