9fa1d7c16205a12eb4023ad8fdd59cfd90bca341
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2023-03-28 15:55:22 +0800

    Update loginService.js

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,7 +2,7 @@ import domainSource from "../common/domain-source";
 
 
 const loginFunction = (data) => {
-    return domainSource.post('login/', data)
+    return domainSource.post('login/', data, {withCredentials: true})
 }
 
 export default {loginFunction}
