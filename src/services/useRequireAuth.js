4c45c48da1702c119b0e8858b6421c28d59cc573
JosephTamayo <josephgabriel.tamayo.iics@ust.edu.ph>
2023-03-30 20:59:05 +0800

    Update useRequireAuth.js

--- src/services/useRequireAuth.js
+++ src/services/useRequireAuth.js
@@ -3,16 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 
 export const useRequireAuth = (allowedRoles) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   
-    useEffect(() => {
-      const role = sessionStorage.getItem("role");
-      if (!role) {
-        navigate("/login");
-        toast.error("Unauthorized Access. Please login again to view.");
-      } else if (allowedRoles && !allowedRoles.includes(role)) {
-        navigate("/");
-        toast.error(`Access denied. You need to be an ${allowedRoles.join(" or ")} to access this page.`);
-      }
-    }, [navigate, allowedRoles]);
-  };
+  useEffect(() => {
+    const role = sessionStorage.getItem("role");
+    if (!role) {
+      navigate("/login");
+      toast.error("Unauthorized Access. Please login again to view.");
+    } else if (allowedRoles && !allowedRoles.includes(role)) {
+      navigate("/");
+      toast.error(`Access denied. You need to be an ${allowedRoles.join(" or ")} to access this page.`);
+    }
+
+    // prevent user from going back to login page
+    window.history.pushState(null, "", window.location.href);
+    window.onpopstate = function () {
+      window.history.pushState(null, "", window.location.href);
+    };
+  }, [navigate, allowedRoles]);
+};
